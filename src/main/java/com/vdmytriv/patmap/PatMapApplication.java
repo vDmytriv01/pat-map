9aae71dbf5e9070027484ac7d8ee3d47aaa46aef
Vadym Dmytriv <vadymdmytriv@gmail.com>
2025-10-19 13:19:02 +0200

    feat: initialize backend structure

---
+++ src/main/java/com/vdmytriv/patmap/PatMapApplication.java
@@ -0,0 +1,12 @@
+package com.vdmytriv.patmap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatMapApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PatMapApplication.class, args);
+    }
+}
